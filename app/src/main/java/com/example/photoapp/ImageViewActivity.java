3bb7d685ce0878d1287cb0157f827c04832e78dd
Jin Sakuma <j.sakuma@jxpress.net>
2019-06-09 12:14:00 +0900

    add pinch in/out

--- app/src/main/java/com/example/photoapp/ImageViewActivity.java
+++ app/src/main/java/com/example/photoapp/ImageViewActivity.java
@@ -3,12 +3,16 @@ package com.example.photoapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class ImageViewActivity extends Activity {
+    private float scale = 1f;
+    private ScaleGestureDetector detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +24,42 @@ public class ImageViewActivity extends Activity {
         Intent intent = getIntent();
         Integer data = intent.getIntExtra("ImageData", 0);
 
-        ImageView imageView = findViewById(R.id.image_view);
+        final ImageView imageView = findViewById(R.id.image_view);
         imageView.setImageResource(data);
+
+        class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+            @Override
+            public boolean onScale(ScaleGestureDetector detector) {
+                scale *= detector.getScaleFactor();
+                imageView.setScaleX(scale);
+                imageView.setScaleY(scale);
+                return true;
+            }
+
+        }
+
+        detector = new ScaleGestureDetector(this,new ScaleListener());
 
 
         Button button = findViewById(R.id.exitButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 拡大した画像をタップするともとの画面に戻る
+                // EXITボタンをタップするともとの画面に戻る
                 Intent intent = new Intent();
                 setResult(RESULT_OK, intent);
                 finish();
             }
         });
+
     }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        //re-route the Touch Events to the ScaleListener class
+        detector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+
 
 }
